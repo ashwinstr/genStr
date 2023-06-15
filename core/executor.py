c26fd21d8056274b50ac819557388cee75d218f5
Kakashi <ashwinstr@gmail.com>
2023-06-15 23:37:57 +0530

    converted command trigger and owner id to integer

--- core/executor.py
+++ core/executor.py
@@ -7,15 +7,17 @@ from helpers.config import Config
 
 
 @bot.on_message(
-    filters.user(Config.OWNER_ID)
-    & filters.command("eval", prefixes=Config.CMD_TRIGGER),
+    filters.user([Config.OWNER_ID])
+    & filters.command("eval", prefixes=[Config.CMD_TRIGGER]),
     group=-2
 )
 async def evaluator(_, message: Message):
     """ code evaluator """
-    text = " ".join(message.text.split()[:-1])
+    text = " ".join(message.text.split()[:-1]) if message.text else None
+    if not text:
+        return await message.reply("`Text not found...\nGive input to run commands.`")
     try:
         evaluated = eval(text)
         await message.reply(evaluated)
     except Exception as e:
-        return await bot.send_message(Config.LOG_CHANNEL_ID, str(e))
+        await bot.send_message(Config.LOG_CHANNEL_ID, str(e))
--- helpers/config.py
+++ helpers/config.py
@@ -1,12 +1,17 @@
 # config.py
+
 import os
+import dotenv
+
+if os.path.isfile("config.env"):
+    dotenv.load_dotenv("config.env")
 
 
 class Config:
 
     API_HASH = os.environ.get("API_HASH")
-    API_ID = int(os.environ.get("API_ID"))
+    API_ID = int(os.environ.get("API_ID", 0))
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
     CMD_TRIGGER = os.environ.get("CMD_TRIGGER")
-    LOG_CHANNEL_ID = int(os.environ.get("LOG_CHANNEL_ID"))
+    LOG_CHANNEL_ID = int(os.environ.get("LOG_CHANNEL_ID", 0))
     OWNER_ID = int(os.environ.get("OWNER_ID", 0))
